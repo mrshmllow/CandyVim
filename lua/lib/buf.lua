f54f4ff7820fd47233c58ec481ec8ba06b30e6d7
mrshmllow <marshycity@gmail.com>
2022-09-29 17:01:34 +1000

    add padding to modified string

--- lua/lib/buf.lua
+++ lua/lib/buf.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.get_modified_string(bufn)
 	if vim.bo[bufn].modified then
-		local modified_icon = "●"
+		local modified_icon = "● "
 		return modified_icon
 	else
 		return ""
