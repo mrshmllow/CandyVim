98849f91e1bcf4df5286212e241162d42b8c5edb
mrshmllow <marshycity@gmail.com>
2022-10-03 19:02:20 +1100

    improve paths & cloning of git tree

--- utils/install.sh
+++ utils/install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-declare INSTALL_PATH="$HOME/.local/share/candyvim_new"
-
 XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 
-CANDYVIM_RUNTIME_DIR="${CANDYVIM_RUNTIME_DIR:-"$XDG_DATA_HOME/candyvim_new"}"
+CANDYVIM_RUNTIME_DIR="${CANDYVIM_RUNTIME_DIR:-"$XDG_DATA_HOME/candyvim"}"
 CANDYVIM_CONFIG_DIR="${CANDYVIM_CONFIG_DIR:-"$XDG_CONFIG_HOME/cvim"}"
 CANDYVIM_CACHE_DIR="${CANDYVIM_CACHE_DIR:-"$XDG_CACHE_HOME/cvim"}"
 
@@ -15,21 +13,21 @@ curl -s https://raw.githubusercontent.com/mrshmllow/CandyVim/main/utils/pretty/m
 
 echo "Installing CandyVim"
 
-rm -rf "$INSTALL_PATH/cvim"
-
-if [ ! -d "$INSTALL_PATH/cvim" ]; then
-  git clone https://github.com/mrshmllow/candyvim --depth 1 "$INSTALL_PATH/cvim"
+if [ ! -d "$CANDYVIM_RUNTIME_DIR/cvim" ]; then
+  git clone https://github.com/mrshmllow/candyvim --depth 1 "$CANDYVIM_RUNTIME_DIR/cvim"
+else
+  git -C "$CANDYVIM_RUNTIME_DIR/cvim" pull
 fi
 
 echo "Installing packer.nvim"
 
-rm -rf "$INSTALL_PATH/site/pack/packer/start/packer.nvim"
+rm -rf "$CANDYVIM_RUNTIME_DIR/site/pack/packer/start/packer.nvim"
 
-git clone https://github.com/wbthomason/packer.nvim --depth 1 "$INSTALL_PATH/site/pack/packer/start/packer.nvim/" --quiet
+git clone https://github.com/wbthomason/packer.nvim --depth 1 "$CANDYVIM_RUNTIME_DIR/site/pack/packer/start/packer.nvim/" --quiet
 
 echo "Copying files..."
 
-source="$INSTALL_PATH/cvim/utils/bin/cvim.template"
+source="$CANDYVIM_RUNTIME_DIR/cvim/utils/bin/cvim.template"
 dest="$HOME/.local/bin/cvim"
 
 rm -f "$dest"
@@ -41,7 +39,7 @@ sed -e s"#RUNTIME_DIR_VAR#\"${CANDYVIM_RUNTIME_DIR}\"#"g \
   -e s"#CACHE_DIR_VAR#\"${CANDYVIM_CACHE_DIR}\"#"g "$source" \
   | tee "$dest" > /dev/null
 
-cp "$INSTALL_PATH/cvim/utils/config.lua.template" "$XDG_CONFIG_HOME/config.lua"
+cp "$CANDYVIM_RUNTIME_DIR/cvim/utils/config.lua.template" "$XDG_CONFIG_HOME/config.lua"
 
 echo "Updating Plugins..."
 
